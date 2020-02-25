73ce706ee603f44cd9cf6d894a6194570e1f7b80
Garcia, Josue <josue.garcia@icfnext.com>
2020-02-25 08:36:02 -0600

    Install snapshots

--- __tests__/button.test.js
+++ __tests__/button.test.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Button from '../button';
+import renderer from 'react-test-renderer';
 
 describe("When component is mounted", () => {
-    it("Then is should contain a button", () => {
+    it("Then it should contain a button", () => {
         const wrapper = shallow(<Button text="Click Me !" onClick={() => {}} />);
         const button = wrapper.find('button');
 
         expect(button.exists()).toEqual(true);
+    });
+
+    it("And it should match snapshot", () => {
+        const json = renderer.create(<Button text="Click Me !" onClick={() => {}} />);
+        
+        expect(json).toMatchSnapshot();
     });
 });
