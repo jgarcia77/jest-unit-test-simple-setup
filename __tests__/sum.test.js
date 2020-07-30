6dca7f106f26c04e61e7f5c8feac81b236360e6a
JoshGa <JoshGa@JOSHGA-PC>
2020-07-30 09:03:22 -0500

    Sum unit test

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,10 +1,12 @@
 import Sum from "../sum";
 
-describe('When adding to positive numbers', () => {
-    it('Then it should be a positive number', () => {
-        const result = Sum(1, 2);
-        const isPositive = result > -1;
-
-        expect(isPositive).toEqual(true);
+describe("Feature: Sum module", () => {
+    describe("Given all arguments are provided", () => {
+        describe("When summing 1 and 2", () => {
+            it("Then it should return 3", () => {
+                const result = Sum(1, 2);
+                expect(result).toEqual(3);
+            });
+        });
     });
 });
